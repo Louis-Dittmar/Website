79f1db9a2f08b300854d64148555bdcee16fbc9e
Louis <louis.dittmar@gmx.de>
2022-05-12 20:03:48 +0200

    Copyright & Meta Updates

--- js/ToDoList.js
+++ js/ToDoList.js
@@ -1,3 +1,21 @@
+/*
+Copyright (c) 2022 by Louis Dittmar
+This program is free software: you can redistribute it and/or modify
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  */
+
+
 var input = document.getElementById("userInput");
 var inputRemove = document.getElementById("ListElement");
 var list = document.getElementById("MyList")
@@ -40,7 +58,7 @@ function MyRemove(el) {
   if (checkbox.checked) {
     parent.remove();
   } else {
-    alert("Du musst erst die Checkbox Aktivieren!")
+    alert("You must first activate the checkbox!")
   }
 }
 
@@ -58,7 +76,7 @@ function DelAllTrue() {
 function DelAll() {
   let value = list.children.length;
   if (value === 0) {
-    alert("Keine Elemente zum Löschen vorhanden!")
+    alert("No elements to delete!")
   } else {
     for (let i = value; i > 0; i--) {
       const RemObj = list.children[i - 1].remove();
