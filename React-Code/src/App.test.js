f54e04df0d3377d8dcad774bfeb9c02e94dc9cc6
Louis <louis.dittmar@gmx.de>
2025-06-03 21:38:53 +0200

    Update App.test.js to check ToDo List heading

--- React-Code/src/App.test.js
+++ React-Code/src/App.test.js
@@ -1,8 +1,8 @@
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App/>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders ToDo List heading', () => {
+  render(<App />);
+  const headingElement = screen.getByText(/ToDo List/i);
+  expect(headingElement).toBeInTheDocument();
 });
